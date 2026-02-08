36d8ea6b70e965b01f190606da5fa7fd3430b57f
asilvainnovations <132311723+asilvainnovations@users.noreply.github.com>
2026-02-09 07:22:09 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import AdminDashboard from "./components/admin/AdminDashboard"; // Import the new dashboard
 
 const queryClient = new QueryClient();
 
@@ -19,6 +19,8 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            {/* Added Admin Route */}
+            <Route path="/admin" element={<AdminDashboard />} /> 
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
